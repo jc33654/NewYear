4eb21b1b6a4e4ed0d37157b61179586bc5db671b
jc33654 <49902604+jc33654@users.noreply.github.com>
2020-01-22 14:27:48 +0800

    #01
    first part

---
+++ NewYear.js
@@ -0,0 +1,20 @@
+var NewYearText = [{
+    name: "佳瑩",
+    passWord: "Ingrid0712",
+    text: `我親愛的妹妹啊～<br>
+            今年要環保愛地球，所以`
+}, {
+    name: "",
+    passWord: "123",
+    text: `<div>123321123
+    123321123
+    123321123
+            </div>`
+}, {
+    name: "",
+    passWord: "123",
+    text: `<div>123321123
+    123321123
+    123321123
+            </div>`
+}];
